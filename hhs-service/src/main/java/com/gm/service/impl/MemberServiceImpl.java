b6f4657d25021a24b3de716673b39f7ba5593ecf
lq <lq@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-05-30 03:13:34 +0000

    上家 上上家 代码重构

--- hhs-service/src/main/java/com/gm/service/impl/MemberServiceImpl.java
+++ hhs-service/src/main/java/com/gm/service/impl/MemberServiceImpl.java
@@ -6,12 +6,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.sse.Sse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +24,6 @@ import com.gm.base.consts.Const;
 import com.gm.base.dao.IBaseDao;
 import com.gm.base.dao.IMemberDao;
 import com.gm.base.dao.ITenReturnOneDao;
-import com.gm.base.dto.Node;
 import com.gm.base.dto.SuperTree;
 import com.gm.base.model.Commodity;
 import com.gm.base.model.Member;
@@ -197,36 +193,20 @@ public class MemberServiceImpl extends BaseServiceImpl<Member, Integer> implemen
 	}
 
 	@Override
-	public Member getParent1(Member member)
-	{// 获取张三的推荐人 50 三级代理 上家
-		Member parent1 = getOne("generalizeId", member.getReferrerGeneralizeId());
-		return parent1;
+	public Member getParent(Member member, int level)
+	{
+		if (level == 1)
+			return doGetParent(member);
+		Member parent = doGetParent(member);
+		if (parent == null)
+			return null;
+		return getParent(parent, --level);
 	}
 
-	@Override
-	public List<Member> getParent2(Member member)
-	{// 获取张三的推介人的推介人 60 二级代理 上上家
-		Member parent = getParent1(member);
-		List<Member> list = new ArrayList<>();
-		if (parent != null) {
-			Member parent2 = getOne("generalizeId", parent.getReferrerGeneralizeId());
-			list.add(parent2);
-		}
-		return list;
-	}
-
-	@Override
-	public List<Member> getParent3(Member member)
-	{// 获取张三推荐人的推荐人的推荐人 50 一级代理 上上上家
-		List<Member> parent2 = getParent2(member);
-		List<Member> list = new ArrayList<>();
-		for (Member member2 : parent2) {
-			if (member2 == null)
-				continue;
-			Member parent3 = getOne("generalizeId", member2.getReferrerGeneralizeId());
-			list.add(parent3);
-		}
-		return list;
+	private Member doGetParent(Member member)
+	{
+		Member parent = getOne("generalizeId", member.getReferrerGeneralizeId());
+		return parent;
 	}
 
 	@Override
@@ -290,22 +270,7 @@ public class MemberServiceImpl extends BaseServiceImpl<Member, Integer> implemen
 
 		if (member.getSetMeal() == 3) { // 直推十人
 			
-			List<Member> parent2 = getParent2(member);
-			List<Member> parent3 = getParent3(member);
-			
-			for (Member member2 : parent2) {
-				if (!StringUtils.isEmpty(member2) ) {
-					list = new ArrayList<>();
-					list.add(member2);
-				}
-			}
-			
-			for (Member member3 : parent3) {
-				if (!StringUtils.isEmpty(member3) ) {
-					list = new ArrayList<>();
-					list.add(member3);
-				}
-			}
+			;
 
 		}
 
@@ -333,7 +298,7 @@ public class MemberServiceImpl extends BaseServiceImpl<Member, Integer> implemen
 		List<Member> list = null;
 
 		if (member.getSetMeal() == 2) { // 购买套餐了
-			Member parent1 = getParent1(member);
+			Member parent1 = getParent(member, 1);
 			if (parent1 != null)
 				list.add(parent1);
 		}
@@ -355,9 +320,9 @@ public class MemberServiceImpl extends BaseServiceImpl<Member, Integer> implemen
 	public void threeLevel(Integer memberId) {
 
 		Member member = dao.getOne("id", memberId);
-		Member parent1 = getParent1(member);
-		List<Member> parent2 = getParent2(member);
-		List<Member> parent3 = getParent3(member);
+		Member parent1 = getParent(member, 1);
+		Member parent2 = getParent(member, 2);
+		Member parent3 = getParent(member, 3);
 
 		if (parent1 != null) {
 			parent1.setBalance(parent1.getBalance().add(BigDecimal.valueOf(50)));
@@ -366,24 +331,20 @@ public class MemberServiceImpl extends BaseServiceImpl<Member, Integer> implemen
 			update(parent1);
 		}
 
-		for (Member member2 : parent2) {
-			if (member2 != null) {
-				member2.setBalance(member2.getBalance().add(BigDecimal.valueOf(50)));
-				logger.info("threeLevel:Member member1 = {}",JSON.toJSON(member2.getId()));
-				logger.info("threeLevel:Member member1 = {}",JSON.toJSON(member2.getNickname()));
-				update(member2);
-				
-			}
+		if (parent2 != null) {
+			parent2.setBalance(parent2.getBalance().add(BigDecimal.valueOf(60)));
+			logger.info("threeLevel:Member member1 = {}",JSON.toJSON(parent2.getId()));
+			logger.info("threeLevel:Member member1 = {}",JSON.toJSON(parent2.getNickname()));
+			update(parent2);
+
 		}
-		
-		for (Member member3 : parent3) {
-			if (member3 != null) {
-				member3.setBalance(member3.getBalance().add(BigDecimal.valueOf(50)));
-				logger.info("threeLevel:Member member1 = {}",JSON.toJSON(member3.getId()));
-				logger.info("threeLevel:Member member1 = {}",JSON.toJSON(member3.getNickname()));
-				update(member3);
-				
-			}
+
+		if (parent3 != null) {
+			parent3.setBalance(parent3.getBalance().add(BigDecimal.valueOf(50)));
+			logger.info("threeLevel:Member member1 = {}",JSON.toJSON(parent3.getId()));
+			logger.info("threeLevel:Member member1 = {}",JSON.toJSON(parent3.getNickname()));
+			update(parent3);
+
 		}
 	}
 
