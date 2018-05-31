ce4686d688a63842eab970743787f135e3eb2093
lq <lq@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-05-31 09:22:26 +0000

    删除测试代码

--- hhs-root/hhs-main/src/main/java/com/gm/Main.java
+++ hhs-root/hhs-main/src/main/java/com/gm/Main.java
@@ -1,7 +1,5 @@
 package com.gm;
 
-import javax.annotation.Resource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,21 +8,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.gm.service.IMemberService;
-
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
 @Controller
 public class Main extends SpringBootServletInitializer {
 
-	@Resource
-	private IMemberService memberService;
-
 	/**
 	 * 用外部tomcat启动
 	 */
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Main.class);
@@ -34,21 +26,5 @@ public class Main extends SpringBootServletInitializer {
 		SpringApplication.run(Main.class, args);
 	}
 
-	// @RequestMapping("/test/{id}")
-	// @ResponseBody
-	// public String index(@PathVariable Integer id) {
-	// Member m = memberService.get(id);
-	// System.out.println("1111111111111111111111111111111111");
-	// JSON.toJSON(m);
-	// List<Member> members = memberService.getAllSons(m);
-	// // for (Member member : members) {
-	// // System.out.println(member.getId());
-	// // }
-	// // List<Member> members1 = memberService.getSons1(m);
-	// // for (Member member : members1) {
-	// // System.err.println(member.getId());
-	// // }
-	// return JSON.toJSONString(members);
-	// }
 
 }
