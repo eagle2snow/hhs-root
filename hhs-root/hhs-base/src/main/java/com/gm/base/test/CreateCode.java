e043cebe6c26f9bf7d7a2094e73589b74d152974
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-05 09:00:55 +0000

    添加拉布达表达式依赖

--- hhs-root/hhs-base/src/main/java/com/gm/base/test/CreateCode.java
+++ hhs-root/hhs-base/src/main/java/com/gm/base/test/CreateCode.java
@@ -2,35 +2,37 @@ package com.gm.base.test;
 
 import java.io.IOException;
 
+import com.gm.base.model.MemberBillDetails;
 import com.gm.gencode.Build;
-
 import freemarker.template.TemplateException;
+
 
 public class CreateCode {
 
-	public static void main(String[] args) throws IOException, TemplateException {
+    public static void main(String[] args) throws IOException, TemplateException {
 
-		/** ====================================================== **/
-		/*
-		 * 生成选项 idao,dao,si,is,c,add,update,list ，null则全部生成
-		 * ======================================================
-		 **/
+        /** ====================================================== **/
+        /*
+         * 生成选项 idao,dao,si,is,c,add,update,list ，null则全部生成
+         * ======================================================
+         **/
 
-		// Build.buildCode(Notice.class, "idao,dao,si,is,c,add,update,list");
-		// Build.buildCode(News.class, "idao,dao,si,is,c,add,update,list");
+        // Build.buildCode(Notice.class, "idao,dao,si,is,c,add,update,list");
+        // Build.buildCode(News.class, "idao,dao,si,is,c,add,update,list");
 
-		// Build.buildCode(AppInfo.class, "idao,dao,si,is,c,add,update,list");
-		// Build.buildCode(MemberAddress.class, null);
+        // Build.buildCode(AppInfo.class, "idao,dao,si,is,c,add,update,list");
+        // Build.buildCode(MemberAddress.class, null);
 
-		// Build.buildCode(AutoMsgType.class, null);
-		// Build.buildCode(AutoMsg.class, null);
-		// Build.buildCode(CommodityEvaluation.class, null);
-		// Build.buildCode(OrderItem.class, "idao,dao,si,is");
+        // Build.buildCode(AutoMsgType.class, null);
+        // Build.buildCode(AutoMsg.class, null);
+        // Build.buildCode(CommodityEvaluation.class, null);
+        // Build.buildCode(OrderItem.class, "idao,dao,si,is");
 
-		// Build.buildCode(TenReturnOne.class, "idao,dao,si,is");
+        //     Build.buildCode(TenReturnOne.class, "idao,dao,si,is");
 
-		// Build.buildCode(TestTime.class, "update");
+//		 Build.buildCode(MemberBillDetails.class, null);
 
-	}
+
+    }
 
 }
