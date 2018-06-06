87692e5852780eb07fd988f88ee8318de9e35e5d
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-06 09:11:58 +0000

    新增会员账单及其相关设置

---
+++ hhs-root/hhs-base/src/main/java/com/gm/base/dao/impl/MemberAccountBillDaoImpl.java
@@ -0,0 +1,19 @@
+package com.gm.base.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.gm.base.dao.IMemberAccountBillDao;
+import com.gm.base.model.MemberAccountBill;
+import com.gm.base.dao.impl.BaseDaoImpl;
+
+/**
+ * 代码生成器生成代码<br/>
+ * MemberAccountBillDaoImpl
+ * @author guet
+ *
+ */
+
+@Repository
+public class MemberAccountBillDaoImpl extends BaseDaoImpl<MemberAccountBill, Integer> implements IMemberAccountBillDao {
+
+}
---
+++ hhs-root/hhs-base/src/main/java/com/gm/base/model/MemberAccountBill.java
@@ -0,0 +1,48 @@
+package com.gm.base.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.gm.gencode.annotation.FormField;
+import com.gm.gencode.annotation.M;
+import com.gm.gencode.util.FieldType;
+
+import lombok.Data;
+
+@Data
+@M("会员账户账单")
+@SuppressWarnings("serial")
+@Entity(name = "memberAccountBill")
+@Table(name = "t_member_acount_bill")
+public class MemberAccountBill extends Model {
+
+	@FormField(type = FieldType.NUMBER, label = "自己Id")
+	private Integer selfId;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "上家ID")
+	private Integer upId;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "上家姓名")
+	private String upName;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "下家ID")
+	private Integer nextId;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "下家姓名")
+	private String nextName;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "订单ID")
+	private String orderNo;
+
+	@FormField(type = FieldType.NUMBER, label = "类型", data = "1|十返一,2|返套餐,3|5元/人,4|提成,6|三级分润,7|买商品,8|买套餐,9|提现")
+	private Integer type;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "金额")
+	private BigDecimal money;
+
+	@FormField(type = FieldType.TEXTINPUT, label = "备注")
+	private String remark;
+
+}
