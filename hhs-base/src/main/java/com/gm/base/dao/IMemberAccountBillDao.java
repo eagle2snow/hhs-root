c703c1dbe6c7f144beb42def74d0addcb0cabbdf
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-06 09:11:58 +0000

    新增会员账单及其相关设置

---
+++ hhs-base/src/main/java/com/gm/base/dao/IMemberAccountBillDao.java
@@ -0,0 +1,15 @@
+package com.gm.base.dao;
+
+import com.gm.base.dao.IBaseDao;
+import com.gm.base.model.MemberAccountBill;
+
+
+/**
+ * 代码生成器生成代码<br/>
+ * IMemberAccountBillDao
+ * @author guet
+ *
+ */
+public interface IMemberAccountBillDao extends IBaseDao<MemberAccountBill, Integer> {
+
+}
