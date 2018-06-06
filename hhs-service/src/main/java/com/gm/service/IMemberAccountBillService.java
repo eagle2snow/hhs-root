6569d2bcce8249b66e4b60f18fc70ff999eeb937
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-06 09:12:57 +0000

    新增会员账单及其相关设置

---
+++ hhs-service/src/main/java/com/gm/service/IMemberAccountBillService.java
@@ -0,0 +1,12 @@
+package com.gm.service;
+import com.gm.service.IBaseService;
+import com.gm.base.model.MemberAccountBill;
+/**
+ * 代码生成器生成代码<br/>
+ * IMemberAccountBillService
+ * @author guet
+ *
+ */
+public interface IMemberAccountBillService extends IBaseService<MemberAccountBill, Integer> {
+
+}
