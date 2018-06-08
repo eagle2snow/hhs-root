07c2d341406f5044aaba938cc9f5b77448cf0c3d
lq <lq@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-08 08:12:03 +0000

    图片操作

--- hhs-main/src/main/java/com/gm/wx/controller/WxCommodityCommentsController.java
+++ hhs-main/src/main/java/com/gm/wx/controller/WxCommodityCommentsController.java
@@ -17,10 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.alibaba.fastjson.JSON;
 import com.gm.service.ICommodityAppraiseService;
@@ -29,6 +26,7 @@ import com.gm.service.IOrderItemService;
 import com.gm.service.IOrderService;
 import com.gm.service.impl.CommodityAppraiseServiceImpl;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.AbstractMultipartHttpServletRequest;
 
 /**
  * @author eagle
@@ -151,7 +149,7 @@ public class WxCommodityCommentsController extends WeixinBaseController {
 
         Commodity commodity = commodityService.get(commodityid);
         Order order = orderService.get(orderItem.getOrder().getId());
-        Member member = getDBMember();
+        Member member = WXHelper.getMember(getCurMember());
 
         if (!member.getId().equals(order.getMember().getId())) {
             logger.error("!curMember.getId().equals(order.getMember().getId())");
