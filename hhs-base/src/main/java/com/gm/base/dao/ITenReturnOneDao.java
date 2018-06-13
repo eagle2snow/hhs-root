eb73b1a89830d88322a63e5bccb6035ad8d108a6
lyz <lyz@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-13 14:54:49 +0000

    增加商品十返一查询语句

--- hhs-base/src/main/java/com/gm/base/dao/ITenReturnOneDao.java
+++ hhs-base/src/main/java/com/gm/base/dao/ITenReturnOneDao.java
@@ -12,4 +12,24 @@ import com.gm.base.model.TenReturnOne;
  */
 public interface ITenReturnOneDao extends IBaseDao<TenReturnOne, Integer> {
 
+	/**
+	 * 查询购买次数
+	 * @param id
+	 * @return
+	 */
+	TenReturnOne selectTime(Integer id);
+	/**
+	 * 查询返还次数
+	 * @param id
+	 * @return 
+	 */
+	TenReturnOne selectTenTime(Integer id);
+	/**
+	 * 查询十返一会员
+	 * @param id
+	 * @param tentime
+	 * @return 
+	 */
+	TenReturnOne selectOneMember(Integer id, int tentime);
+
 }
