be71ed372eda33923e22eeee876ed7e4c87f0c2b
lyz <lyz@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-13 14:54:39 +0000

    增加商品十返一查询语句

--- hhs-base/src/main/java/com/gm/base/dao/impl/TenReturnOneDaoImpl.java
+++ hhs-base/src/main/java/com/gm/base/dao/impl/TenReturnOneDaoImpl.java
@@ -16,4 +16,22 @@ import com.gm.base.dao.impl.BaseDaoImpl;
 @Repository
 public class TenReturnOneDaoImpl extends BaseDaoImpl<TenReturnOne, Integer> implements ITenReturnOneDao {
 
+	@Override
+	public TenReturnOne selectTime(Integer id) {
+		return getOne("SELECT MAX(time) FROM t_ten_return_one WHERE this_time_commodity_id = "+id);
+	}
+
+	@Override
+	public TenReturnOne selectTenTime(Integer id) {
+		return getOne("SELECT MAX(ten_time) FROM t_ten_return_one WHERE this_time_commodity_id = "+id);
+		
+	}
+
+	@Override
+	public TenReturnOne selectOneMember(Integer id, int tentime) {
+		int mun = tentime - 1;
+		return getOne("SELECT id,this_time_member_id FROM t_ten_return_one WHERE this_time_commodity_id="+id+"  ORDER BY id ASC LIMIT "+mun+",1");
+		
+	}
+
 }
