add8bf1aa4401ad24495f4097ee5ad60bb66b9ed
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-14 09:36:56 +0000

    ① 推广费数据动态展示

--- hhs-main/src/main/java/com/gm/wx/controller/WxMyCenterMemberBillController.java
+++ hhs-main/src/main/java/com/gm/wx/controller/WxMyCenterMemberBillController.java
@@ -1,5 +1,9 @@
 package com.gm.wx.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -17,6 +21,7 @@ import com.gm.base.controller.BaseController;
 import com.gm.base.model.MemberAccountBill;
 import com.gm.service.IMemberAccountBillService;
 import com.gm.service.IMemberService;
+import com.gm.utils.LocalDateUtil;
 
 @Controller
 @RequestMapping("wx/myCenter/")
@@ -38,14 +43,41 @@ public class WxMyCenterMemberBillController extends BaseController {
 	 * @return
 	 */
 	@RequestMapping("memberBill/{memberId}")
-	public String memberBill(@PathVariable Integer memberId,ModelMap modelAndView) {
+	public String memberBill(@PathVariable Integer memberId, ModelMap modelAndView) {
 		logger.info("memberBill:the args memberId = {}", memberId);
 
 		List<MemberAccountBill> accountBills = billService.listEq("selfId", memberId);
 		logger.info("memberBill:List<MemberAccountBill> accountBills = {}", JSON.toJSON(accountBills));
 
-		if (accountBills.size() > 0)
+		if (accountBills.size() > 0) {
 			modelAndView.put("accountBills", accountBills);
+			BigDecimal outManoy = BigDecimal.ZERO;
+			BigDecimal inManoy = BigDecimal.ZERO;
+			for (MemberAccountBill memberAccountBill : accountBills) {
+				//处理时间
+				
+				DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+				String localTime = df.format(memberAccountBill.getCreateTime());
+				System.out.println("LocalDateTime转成String类型的时间："+localTime);
+				modelAndView.put("date", localTime);
+				
+				// 支出 7|买商品,8|买套餐,9|提现
+				if (memberAccountBill.getType() == 7 || memberAccountBill.getType() == 8
+						|| memberAccountBill.getType() == 9) {
+					outManoy = outManoy.add(memberAccountBill.getMoney());
+					modelAndView.put("outManoy", outManoy);
+					System.out.println(outManoy);
+				
+				} else { // 收入
+					inManoy = inManoy.add(memberAccountBill.getMoney());
+					modelAndView.put("inManoy", inManoy);
+					System.out.println(inManoy);
+
+				}
+
+			}
+
+		}
 
 		return PATH + "memberBill";
 	}
