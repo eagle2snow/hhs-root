2a985c6c8c009cbaac2a6bfe438d5412bf5a9d18
lq <lq@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-06-20 10:53:25 +0000

    返5元 2元

--- hhs-main/src/test/java/UnitTest.java
+++ hhs-main/src/test/java/UnitTest.java
@@ -1,6 +1,6 @@
 import com.gm.Main;
 import com.gm.base.model.Member;
-import com.gm.service.impl.OrderServiceImpl;
+import com.gm.service.impl.MemberServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
@@ -15,12 +19,28 @@ public class UnitTest {
 
 
     @Resource
-    OrderServiceImpl orderService;
+    MemberServiceImpl memberService;
 
     @Test
-    @Transactional
     public void foo()
     {
-        
+        Member member = memberService.get(8);
+        MemberServiceImpl.Count count = memberService.new Count();
+        List<Integer> chains = new ArrayList<>();
+        Member root = count.getRoot(member, chains);
+        count.visit(root);
+        Map<Integer, Integer> childrenCount = count.getChildrenCount();
+        Map<Integer, Integer> direct = count.getDirect();
+        for (Integer one : chains) {
+            if (one.equals(1)) {
+                Member member1 = memberService.get(one);
+                member1.setBalance(member1.getBalance().add(BigDecimal.valueOf(10)));
+                boolean update = memberService.update(member1);
+                System.out.println("update = " + update);
+            }
+        }
+
+
+        System.out.println(root);
     }
 }
